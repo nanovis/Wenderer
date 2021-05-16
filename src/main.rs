133b2ed58f9eb374f2074ca5a8114972ad3dbb28
feng liang <feng.liang@kaust.edu.sa>
2021-05-16 21:10:56 +0300

    changed to use build script to build shaders beforehand

--- src/main.rs
+++ src/main.rs
@@ -52,39 +52,10 @@ impl State {
             present_mode: wgpu::PresentMode::Fifo,
         };
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
-        let vs_src = include_str!("shaders/shader.vert");
-        let fs_src = include_str!("shaders/shader.frag");
-        let mut compiler = shaderc::Compiler::new().unwrap();
-        let vs_spirv = compiler
-            .compile_into_spirv(
-                vs_src,
-                shaderc::ShaderKind::Vertex,
-                "shader.vert",
-                "main",
-                None,
-            )
-            .unwrap();
-        let fs_spirv = compiler
-            .compile_into_spirv(
-                fs_src,
-                shaderc::ShaderKind::Fragment,
-                "shader.frag",
-                "main",
-                None,
-            )
-            .unwrap();
-        let vs_data = wgpu::util::make_spirv(vs_spirv.as_binary_u8());
-        let fs_data = wgpu::util::make_spirv(fs_spirv.as_binary_u8());
-        let vs_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-            label: Some("Vertex Shader"),
-            source: vs_data,
-            flags: wgpu::ShaderFlags::default(),
-        });
-        let fs_module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-            label: Some("Fragment Shader"),
-            source: fs_data,
-            flags: wgpu::ShaderFlags::default(),
-        });
+        let vs_module =
+            device.create_shader_module(&wgpu::include_spirv!("shaders/shader.vert.spv"));
+        let fs_module =
+            device.create_shader_module(&wgpu::include_spirv!("shaders/shader.frag.spv"));
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
