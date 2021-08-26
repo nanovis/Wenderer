1d1f1a2e809e461557d2abe1a0114f1700dd9cb9
Feng Liang <feng.liang@kaust.edu.sa>
2021-08-26 16:29:28 +0300

    bring back window builder

--- src/main.rs
+++ src/main.rs
@@ -321,7 +321,11 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
 fn main() {
     let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_inner_size(PhysicalSize::new(1000, 1000))
+        .with_title("WebGPU-based DVR")
+        .build(&event_loop)
+        .unwrap();
     #[cfg(not(target_arch = "wasm32"))]
         {
             env_logger::init();
