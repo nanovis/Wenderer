50e6773791234f0353e98b7b71331cde476749da
Feng Liang <feng.liang@kaust.edu.sa>
2022-07-02 17:24:55 +0300

    fix typo

--- src/rendering.rs
+++ src/rendering.rs
@@ -12,7 +12,7 @@ use std::num::NonZeroU32;
 // The coordinate system in Wgpu is based on DirectX, and Metal's coordinate systems.
 // That means that in normalized device coordinates the x axis and y axis are in the range of -1.0 to +1.0, and the z axis is 0.0 to +1.0.
 // The cgmath crate (as well as most game math crates) are built for OpenGL's coordinate system.
-// This matrix will scale and translate our scene from OpenGL's coordinate sytem to WGPU's.
+// This matrix will scale and translate our scene from OpenGL's coordinate system to WGPU's.
 // We'll define it as follows.
 #[rustfmt::skip]
 pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
